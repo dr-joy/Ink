df90594882714493bfe5ec44d8afe45c940677ad
vu.dinh <vudv.2508@gmail.com>
2024-12-04 10:14:14 +0700

    Ink 0.6.0

---
+++ Ink/Ink.h
@@ -0,0 +1,18 @@
+//
+//  Ink.h
+//  Ink
+//
+//  Created by Dinh Van Vu on 04/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Ink.
+FOUNDATION_EXPORT double InkVersionNumber;
+
+//! Project version string for Ink.
+FOUNDATION_EXPORT const unsigned char InkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ink/PublicHeader.h>
+
+
